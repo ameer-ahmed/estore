91ec834f1cab79856bc172f87f46567c119802d8
Ameer Ahmed <ameer2001an@gmail.com>
2022-07-01 17:34:56 +0200

    Configured multi-lingual package and tested it

--- app/Http/Controllers/Dashboard/Home/HomeController.php
+++ app/Http/Controllers/Dashboard/Home/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Dashboard\Home;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
---
+++ app/Http/Traits/TranslatableHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+trait TranslatableHelper
+{
+
+}
